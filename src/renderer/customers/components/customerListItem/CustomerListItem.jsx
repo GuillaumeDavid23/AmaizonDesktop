4de20c0f8fd648ec7b929d9e994fdbc6004468b9
Benjamin Rault <b_rault@outlook.fr>
2022-06-29 17:06:49 +0200

    Add Customers components

---
+++ src/renderer/customers/components/customerListItem/CustomerListItem.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
+const CustomerListItem = () => {
+    return (
+        <Box>
+            <Typography>CustomerListItem</Typography>
+        </Box>
+    );
+};
+
+export default CustomerListItem;
---
+++ src/renderer/customers/components/index.js
@@ -0,0 +1,4 @@
+import { CustomerDetails } from "./customerDetails";
+import { CustomerListItem } from "./customerListItem";
+
+export { CustomerDetails, CustomerListItem };
