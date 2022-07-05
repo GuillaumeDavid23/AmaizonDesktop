34a8f05cb5199f960e1eda3acdc9a6c235ba95cd
GuillaumeDavid23 <guillaume.david744@orange.fr>
2022-07-05 15:49:21 +0200

    announce fix

--- src/renderer/announces/components/HomeCards/HomeCards.jsx
+++ src/renderer/announces/components/HomeCards/HomeCards.jsx
@@ -1,26 +1,27 @@
-import { Link } from 'react-router-dom'
-import { useState } from 'react'
-import { BsBorderAll } from 'react-icons/bs'
+import { useNavigate } from 'react-router-dom'
+import { BsBorderAll, BsCheckCircleFill, BsXCircleFill } from 'react-icons/bs'
 import { MdLocationSearching, MdArchitecture } from 'react-icons/md'
 import BtnGeneral from '../../../globalComponents/BtnGeneral/BtnGeneral'
-import { Card } from 'react-bootstrap'
+import { Card, Col, Row } from 'react-bootstrap'
 import './HomeCards.css'
+import { Box } from '@mui/material'
 
 const HomeCards = (props) => {
 	const {
 		_id,
 		imageUrl,
 		title,
-		description,
 		amount,
 		surface,
 		roomNumber,
 		location,
-		transactionType
+		transactionType,
+		isToSell
 	} = props.propertyDatas
+	const navigate = useNavigate()
 
 	return (
-		<Card className="mb-5" id={_id}>
+		<Card className="mb-5 w-75" id={_id}>
 			<Card.Img
 				variant="top"
 				src={window.electron.url + '/' + imageUrl?.photo1}
@@ -29,44 +30,57 @@ const HomeCards = (props) => {
 			/>
 			<Card.Body className="d-flex flex-column">
 				<Card.Title>{title}</Card.Title>
-				<div>
-					<p className="card-text">{description}</p>
-					<div className="text-center">
-						<div className="price ">
-							{amount?.toLocaleString('FR')} €{' '}
-							<small className="location text-secondary">
-								{transactionType === 'Location' ? '/ Mois' : ''}
-							</small>
-						</div>
-						<div className="d-flex justify-content-evenly mt-2">
-							<div className="d-flex flex-column align-items-center fw-bold">
-								<BsBorderAll size={20} />
-								{roomNumber} pièce(s)
-							</div>
-							<div className="d-flex flex-column align-items-center fw-bold">
-								<MdLocationSearching size={20} />
-								{location}
-							</div>
-							<div className="d-flex flex-column align-items-center fw-bold">
-								<MdArchitecture size={20} />
-								{surface} m²
-							</div>
-						</div>
-					</div>
-				</div>
-
-				<div className="d-flex justify-content-center align-items-center">
-					<Link
-						to={`/home`}
-						className="d-flex justify-content-center"
-						style={{ textDecoration: 'none' }}
+				<Row className="d-flex justify-content-evenly mt-2">
+					<Col className="d-flex flex-column align-items-center fw-bold">
+						<BsBorderAll size={20} />
+						{roomNumber} pièce(s)
+					</Col>
+					<Col className="d-flex flex-column align-items-center fw-bold">
+						<MdLocationSearching size={20} />
+						{location}
+					</Col>
+					<Col className="d-flex flex-column align-items-center fw-bold">
+						<MdArchitecture size={20} />
+						{surface} m²
+					</Col>
+				</Row>
+				<Row>
+					<Col
+						xs={12}
+						className="price d-flex justify-content-center"
 					>
+						{amount?.toLocaleString('FR')} €{' '}
+						<small className="location text-secondary ms-1 align-self-end">
+							{transactionType === 'Location' ? '/ Mois' : ''}
+						</small>
+					</Col>
+				</Row>
+				<Row className="align-items-center">
+					<Col xs={12} md={8} lg={6}>
+						{isToSell ? (
+							<Box className="d-flex justify-content-center align-items-center">
+								<BsCheckCircleFill color="green" />{' '}
+								<strong className="ms-2">Dispo</strong>{' '}
+							</Box>
+						) : (
+							<Box className="d-flex justify-content-center align-items-center">
+								<BsXCircleFill color="red" />{' '}
+								<strong className="ms-2">Non dispo</strong>{' '}
+							</Box>
+						)}
+					</Col>
+					<Col xs={12} md={8} lg={6}>
 						<BtnGeneral
-							className="w-100 h-50"
+							onClick={() =>
+								navigate('/singleAnnounce', {
+									state: { property: props.propertyDatas }
+								})
+							}
+							className="w-100"
 							text="Voir l'annonce"
 						/>
-					</Link>
-				</div>
+					</Col>
+				</Row>
 			</Card.Body>
 		</Card>
 	)
