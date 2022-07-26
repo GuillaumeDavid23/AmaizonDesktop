2883175e76a067541498b20621f2fed94408c677
Vincent Mancheron <vmancheron@yahoo.fr>
2022-07-26 14:08:53 +0200

    1-step screen Agents (need modal) + index services

--- src/renderer/agents/Agents.jsx
+++ src/renderer/agents/Agents.jsx
@@ -1,9 +1,12 @@
-import { AnimatedPage } from '../globalComponents'
-import { Box } from '@mui/material'
+import { AnimatedPage, Title } from '../globalComponents'
+import { Box, Button, Grid } from '@mui/material'
 import { useState, useEffect } from 'react'
-import { getAgents } from '../services/Agent'
+import { getAgents } from '../services'
 import { useSlideSnack } from '../hooks'
 import { useNavigate, useLocation } from 'react-router-dom'
+import './Agents.css'
+import { AgentListItem } from './components'
+// import FormControl from 'react-bootstrap/FormControl'
 
 const Agents = () => {
 	// Déclaration useNavigate:
@@ -43,28 +46,48 @@ const Agents = () => {
 
 	return (
 		<AnimatedPage>
-			<Box className="d-flex justify-content-center align-items-center h-100">
-				<ul>
+			<Grid sx={{ height: '100%' }}>
+				<Box sx={{ height: '10%' }}>
+					{/* Component Header */}
+					<Title text="Agents" />
+
+					<Box
+						sx={{
+							display: 'flex',
+							justifyContent: 'flex-end',
+							paddingRight: '50px',
+							marginBottom: '2em'
+						}}
+					>
+						<Button
+							variant="contained"
+							onClick={() => navigate('/createAgent')}
+						>
+							Ajouter un agent
+						</Button>
+					</Box>
+				</Box>
+
+				{/* Customers list */}
+				<Grid
+					container
+					sx={{
+						marginTop: '20px',
+						paddingX: '20px',
+						overflowY: 'scroll',
+						height: '80%'
+					}}
+					spacing={3}
+				>
 					{agents.map((agent) => {
 						return (
-							<li key={agent._id} className="d-flex">
-								<span>
-									{agent.lastname} {agent.firstname}
-								</span>
-								<button
-									onClick={() =>
-										navigate('/createAgent', {
-											state: { id: agent._id }
-										})
-									}
-								>
-									Modifier
-								</button>
-							</li>
+							<Grid item xs={4} key={agent.id}>
+								<AgentListItem agent={agent} />
+							</Grid>
 						)
 					})}
-				</ul>
-			</Box>
+				</Grid>
+			</Grid>
 			{/* Snackbar */}
 			{renderSnack}
 		</AnimatedPage>
@@ -72,3 +95,93 @@ const Agents = () => {
 }
 
 export default Agents
+
+// ? Composant avec REDUX:
+
+// // Initial component States
+// const initialStates = {
+// 	searchCustomer: '',
+// 	users: [],
+// 	filteredUsers: []
+// }
+
+// // Component Actions
+// const actions = {
+// 	INIT_CUSTOMERS: 'INIT_CUSTOMERS',
+// 	SET_CUSTOMERS: 'SET_CUSTOMERS',
+// 	SET_SEARCHCUSTOMER: 'SET_SEARCHCUSTOMER',
+// 	SET_FILTEREDUSERS: 'SET_FILTEREDUSERS'
+// }
+
+// // Component Reducer
+// const CReducer = (states, action) => {
+// 	switch (action.type) {
+// 		case actions.INIT_CUSTOMERS:
+// 			return {
+// 				...states,
+// 				users: action.payload,
+// 				filteredUsers: action.payload
+// 			}
+// 		case actions.SET_CUSTOMERS:
+// 			return { ...states, users: action.payload }
+// 		case actions.SET_SEARCHCUSTOMER:
+// 			return { ...states, searchCustomer: action.payload }
+// 		case actions.SET_FILTEREDUSERS:
+// 			return { ...states, filteredUsers: action.payload }
+
+// 		default:
+// 			return { ...states }
+// 	}
+// }
+
+// // Setting up component Reducer
+// const [states, dispatch] = React.useReducer(CReducer, initialStates)
+// const { searchCustomer, users, filteredUsers } = states
+
+// const handleChange = ({ target: { value } }) => {
+// 	dispatch({ type: actions.SET_SEARCHCUSTOMER, payload: value })
+
+// 	// By default, setting new list to original customers list
+// 	let newCustomerList = [...users]
+
+// 	if (value !== '') {
+// 		newCustomerList = []
+
+// 		// filtering on firstname + lastname + email
+// 		newCustomerList.push(
+// 			...users.filter((user) => user.firstname.includes(value)),
+// 			...users.filter((user) => user.lastname.includes(value)),
+// 			...users.filter((user) => user.email.includes(value))
+// 		)
+
+// 		// Using Set to get UNIQUE customers
+// 		newCustomerList = [...new Set(newCustomerList)]
+// 	}
+
+// 	// Setting filtered customers
+// 	dispatch({
+// 		type: actions.SET_FILTEREDUSERS,
+// 		payload: newCustomerList
+// 	})
+// }
+
+// React.useEffect(() => {
+// 	fetch(`${window.electron.url}/api/user/customers`, {
+// 		headers: {
+// 			Authorization: `bearer ${JSON.parse(
+// 				localStorage.getItem('REACT_TOKEN_AUTH_AMAIZON')
+// 			)}`
+// 		}
+// 	}).then((res) => {
+// 		if (res.ok) {
+// 			res.json().then((data) => {
+// 				const { user: customers } = data
+// 				console.log(customers)
+// 				dispatch({
+// 					type: actions.INIT_CUSTOMERS,
+// 					payload: customers
+// 				})
+// 			})
+// 		}
+// 	})
+// }, [])
---
+++ src/renderer/agents/components/AgentListItem/AgentListItem.jsx
@@ -0,0 +1,148 @@
+import { useState } from 'react'
+import { Box, Button, Grid, Typography } from '@mui/material'
+import Modal from 'react-bootstrap/Modal'
+import { ArrowForward } from '@mui/icons-material'
+import { AgentDetails } from './components'
+import { useNavigate } from 'react-router-dom'
+
+const AgentListItem = ({ agent }) => {
+	// Déclaration useNavigate:
+	let navigate = useNavigate()
+
+	// Modal state
+	const [open, setOpen] = useState(false)
+
+	const handleModalClose = () => {
+		setOpen(false)
+	}
+
+	const handleModalOpen = () => {
+		setOpen(true)
+	}
+
+	return (
+		<Grid
+			container
+			sx={{
+				display: 'flex',
+				flexDirection: 'column',
+				backgroundColor: '#ECE6DE',
+				padding: '10px',
+				borderRadius: '5px',
+				boxShadow: '3px 5px 10px #737373'
+			}}
+		>
+			{/* CustomerInfo Modal */}
+			<Modal
+				show={open}
+				onHide={handleModalClose}
+				style={{ height: '800px' }}
+			>
+				<AgentDetails user={agent} />
+			</Modal>
+			{/* User Informations */}
+			<Grid item sx={{ display: 'flex', flexDirection: 'row' }}>
+				{/* Profil Pic */}
+				<Box
+					sx={{
+						height: '100px',
+						width: '100px',
+						backgroundColor: 'blue',
+						borderRadius: '10px'
+					}}
+				></Box>
+				{/* Agent Info + Prefs */}
+				<Box sx={{ paddingLeft: '10px' }}>
+					<Typography>
+						{agent.firstname} {agent.lastname}
+					</Typography>
+					<Typography>
+						(<i>{agent.email}</i>)
+					</Typography>
+				</Box>
+			</Grid>
+			{/* User Actions */}
+			<Grid
+				item
+				sx={{
+					justifyContent: 'center',
+					width: '100%'
+				}}
+			>
+				<Box
+					sx={{
+						display: 'flex',
+						flexDirection: { sm: 'column', md: 'row' },
+						paddingTop: '10px',
+						overflowY: 'auto',
+						justifyContent: 'space-around',
+						alignItems: 'center'
+					}}
+				>
+					<Button
+						variant="contained"
+						sx={{
+							fontSize: '0.8em',
+							width: { sm: '70%', md: '33%' },
+							backgroundColor: '#647F94',
+							borderRadius: '10px 0px 0px 10px',
+							'&:hover': {
+								backgroundColor: '#647F94'
+							}
+						}}
+						onClick={() =>
+							navigate('/createAgent', {
+								state: { id: agent.id }
+							})
+						}
+					>
+						Modifier
+					</Button>
+					<Button
+						variant="contained"
+						sx={{
+							fontSize: '0.8em',
+							width: { sm: '70%', md: '33%' },
+							backgroundColor: '#647F94',
+							borderRadius: '0px',
+							'&:hover': {
+								backgroundColor: '#647F94'
+							}
+						}}
+						onClick={() =>
+							navigate('/', {
+								state: {
+									snackParams: {
+										message:
+											'Fonctionnalité pas encore développée !',
+										severity: 'warning'
+									}
+								}
+							})
+						}
+					>
+						Rendez-vous
+					</Button>
+					<Button
+						variant="contained"
+						sx={{
+							fontSize: '0.8em',
+							width: { sm: '70%', md: '33%' },
+							backgroundColor: '#647F94',
+							borderRadius: '0px 10px 10px 0px',
+							'&:hover': {
+								backgroundColor: '#647F94'
+							}
+						}}
+						endIcon={<ArrowForward />}
+						onClick={handleModalOpen}
+					>
+						Voir plus
+					</Button>
+				</Box>
+			</Grid>
+		</Grid>
+	)
+}
+
+export default AgentListItem
